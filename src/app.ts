ea60f7591b9fac5d11c840723e7adc991262f409
Bruno Frascino <bfrascino80@gmail.com>
2022-11-03 22:56:57 +1100

    feat: Add cors package for testing FE calls

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import logger from './logger';
 import { ConfigKeys } from './model/utils.model';
 import routes from './routes';
 import * as sqlite from './repository/sqlite';
+import cors from 'cors';
 
 // export NODE_ENV=development (default)
 const port: number = config.get(ConfigKeys.port);
@@ -12,6 +13,8 @@ const host: string = config.get(ConfigKeys.host);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// for testing purposes only
+app.use(cors());
 
 // connect to the database and initialise routes
 sqlite.connect(async () => {
